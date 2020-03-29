f4e7b266aef8808ea103f6dcea7179d0704d2769
jaden hendrickson <hendricksonjaden@yahoo.com>
2020-03-29 14:24:13 -0600

    Links show up in browser and change when clicked. Specified which routes show up with help of route component from react router

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,11 +1,28 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import Navigation from '../Navigation';
+import SignInPage from '../SignIn';
+import PasswordForgetPage from '../PasswordForget';
+import HomePage from '../Home';
+import AccountPage from '../Account';
+import AdminPage from '../Admin';
+
+import * as ROUTES from '../../constants/routes';
 
 const App = () => (
   <Router>
-    <Navigation />
+    <div>
+      <Navigation />
+      
+      <hr />
+
+      <Route path={ROUTES.SIGN_IN} component={SignInPage} />
+      <Route path={ROUTES.PASSWORD_FORGET} component={PasswordForgetPage} />
+      <Route path={ROUTES.HOME} component={HomePage} />
+      <Route path={ROUTES.ACCOUNT} component={AccountPage} />
+      <Route path={ROUTES.ADMIN} component={AdminPage} />
+    </div>
   </Router>
 );
 
