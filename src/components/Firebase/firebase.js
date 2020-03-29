8e8c907d41f7cdd80a6224f9c34dcd444f53b35a
jaden hendrickson <hendricksonjaden@yahoo.com>
2020-03-29 15:14:36 -0600

    configured firebase in the application

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -1,8 +1,18 @@
+import app from 'firebase/app';
+
 const config = {
-  apiKey: AIzaSyCX060lrDCU1gRKjXlhDvQFbHPuBA8NVd8,
-  authDomain: capstone-final-js.firebaseapp.com,
-  databaseURL: "https://capstone-final-js.firebaseio.com",
-  projectId: capstone-final-js,
-  storageBucket: 'capstone-final-js.appspot.com',
-  messagingSenderId: 681158044622,
+  apiKey: process.env.AIzaSyCX060lrDCU1gRKjXlhDvQFbHPuBA8NVd8,
+  authDomain: process.env.capstone-final-js.firebaseapp.com,
+  databaseURL: process.env."https://capstone-final-js.firebaseio.com",
+  projectId: process.env.capstone-final-js,
+  storageBucket: process.env.'capstone-final-js.appspot.com',
+  messagingSenderId: process.env.681158044622,
 };
+
+class Firebase {
+  constructor() {
+    app.initializeApp(config);
+  }
+}
+
+export default Firebase;
