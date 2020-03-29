2bfed1cccc12cc2f09ac53e4f792d722d88b1d43
jaden hendrickson <hendricksonjaden@yahoo.com>
2020-03-29 14:10:11 -0600

    installed react-router-dom

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
-const Home = () => (
+const Landing = () => (
   <div>
-    <h1>Home</h1>
+    <h1>Landing</h1>
   </div>
 );
 
-export default Home;
+export default Landing;
