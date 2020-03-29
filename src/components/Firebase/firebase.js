a463c32de0f641659caebfde1a759798d52f3ff3
jaden hendrickson <hendricksonjaden@yahoo.com>
2020-03-29 15:07:21 -0600

    added firebase component

---
+++ src/components/Firebase/firebase.js
@@ -0,0 +1,8 @@
+const config = {
+  apiKey: AIzaSyCX060lrDCU1gRKjXlhDvQFbHPuBA8NVd8,
+  authDomain: capstone-final-js.firebaseapp.com,
+  databaseURL: "https://capstone-final-js.firebaseio.com",
+  projectId: capstone-final-js,
+  storageBucket: 'capstone-final-js.appspot.com',
+  messagingSenderId: 681158044622,
+};
